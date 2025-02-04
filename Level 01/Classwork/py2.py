c5d3c51ae91173daa3d8cd3ce2277675a442f1b8
nikoloz127000 <nicksmith127000@gmail.com>
2025-02-04 19:02:21 +0400

    commit

---
+++ Level 01/Classwork/py2.py
@@ -0,0 +1,5 @@
+parent1 = input("Enter your name:")
+parent2 = input("Enter your name:")
+
+smth = "Hello my name is Nikoloz my dads name is ",parent1," and my moms name is ",parent2,""
+print( smth)
