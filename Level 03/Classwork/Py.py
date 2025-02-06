80fb24204d1b7599672383805a81ea43a61260f0
nikoloz127000 <nicksmith127000@gmail.com>
2025-02-06 20:47:46 +0400

    commit

---
+++ Level 03/Classwork/Py.py
@@ -0,0 +1,39 @@
+# num = int(input("Enter a number: "))
+# print(5 + num)
+# print(5 - num)
+# print(5 * num)
+# print(5 / num)
+# print(5 // num)
+
+
+
+# print(2 > 1)
+# print(4 < 4)
+# print(7 < 8)
+# print(12 > 16)
+# print(11 == 11)
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a number:"))
+# print(num1 == num2)
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 <=  num2)
+# print(num1 >=  num2)
+# print(num1 != num2)
+
+
+# print(True or False)
+# print(False or True)
+# print(True and False)
+# print(False and True)
+# print(True and True)
+
+
+
+
+# num1 = 5 > 1
+# num2 = 10 > 1
+# print(num1 and num2)
+
+
