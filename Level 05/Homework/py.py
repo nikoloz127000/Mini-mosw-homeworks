0456113b5d0fa3ef70733fdb296d1d66e03bee5f
nikoloz127000 <nicksmith127000@gmail.com>
2025-02-16 21:21:43 +0400

    commit

---
+++ Level 05/Homework/py.py
@@ -0,0 +1,12 @@
+#algorithms --> step by step done instructions togetherness
+
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(50,150):
+#     print(i)
+
+# for loops are used to repeat some operation in a certain amount
+# true and false wll display false because were telling the input to do two things at the same time since it wont work it will display false
+# true or false will display true because were telling it to choose betweene true or false and it will always pick true because it has an option to choose
