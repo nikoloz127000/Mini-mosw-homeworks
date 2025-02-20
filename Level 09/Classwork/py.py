b59292c79e9961e1178db9384456b2279cc28ef4
nikoloz127000 <nicksmith127000@gmail.com>
2025-02-20 21:33:44 +0400

    commit

---
+++ Level 09/Classwork/py.py
@@ -0,0 +1,20 @@
+# for i in range(1,20,2):
+#     print(i)
+# for i in range(0,50,2):
+#     print(i)
+
+
+
+# counter = 10
+# while counter < 15:
+#     print(counter)
+#     counter = counter + 1
+
+# counter = 50
+# while counter > 0:
+#     print(counter)
+#     counter = counter - 1
+
+
+
+# While loops are used when we dont know the count
---
+++ Level 09/Homework/py.py
@@ -0,0 +1,45 @@
+# counter = 0
+# while counter < 10:
+#     print(counter)
+#     counter = counter + 1
+
+
+# counter = 50
+# while counter < 150:
+#     print(counter)
+#     counter = counter  + 1
+
+
+# for i in range(200,500,2):
+#     print(i)
+
+
+# last_name = input("Enter your last name: ")
+# for i in last_name:
+#     print(i)
+
+
+
+# for loop is used when we know the number of iterations
+
+
+
+# counter = 10
+# while counter < 15:
+#     print(counter)
+#     counter = counter + 1
+
+# counter = 50
+# while counter > 0:
+#     print(counter)
+#     counter = counter - 1
+
+#A counter variable is created and initialized to a starting value before the while loop is started
+
+#A while loop is used when we dont know the number of iterations
+
+# for i in range(10,70,2):
+#     print(i)
+
+# for i in range(25,55,2):
+#     print(i)
