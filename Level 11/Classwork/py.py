6402d894bee7c22f34c5ff4edaa4b94533ebb870
nikoloz127000 <nicksmith127000@gmail.com>
2025-02-26 17:53:45 +0400

    commit

---
+++ Level 11/Classwork/py.py
@@ -0,0 +1,14 @@
+# sum = 0
+# for i in range(1,20):
+#     sum = sum + i
+# print(sum)
+# sum = 0 
+# for i in range(1,30):
+#     sum = sum + i
+# print(sum)
+
+
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print("You are an adult")
+# else: print("you are kid")
