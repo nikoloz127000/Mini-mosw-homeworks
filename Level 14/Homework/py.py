83e242956d58b21e40c6dfcff679d932b3da198e
nikoloz127000 <nicksmith127000@gmail.com>
2025-03-01 15:15:43 +0400

    commit

---
+++ Level 14/Homework/py.py
@@ -0,0 +1,11 @@
+# fruits = ["Apple", "Banana", "Orange", "elderBerry", "Berry", "peach", " pepper", "kivi", "avocado"]
+# print(fruits[0:5])
+
+
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list[0]  + list[9]
+
+
+
+# names = ["Nikolozi", "Giorgio", "Vano", "levani", "tea"]
+# print(names[2:5])
