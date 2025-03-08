fcb1c8a9d2a554f13b659ec0bb7f705545501bec
nikoloz127000 <nicksmith127000@gmail.com>
2025-03-08 14:55:40 +0400

    Commit

---
+++ Level 16/Classwork/py.py
@@ -0,0 +1,10 @@
+# names = ["Nikoloz", "Giorgi", "Vano", "Levani"]
+# print(names[-1])
+# print(names[-3:-1])
+# print(names[-3:])
+
+
+
+# names = ["Nikoloz", "Giorgi", "Vano", "Levani"]
+# names[0] = "Luka"
+# print(names)
---
+++ Level 16/Homework/py.py
@@ -0,0 +1,20 @@
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[0] + nums[-1])
+
+
+# slicing is used for taking out how many ever elements you want from the list and printing them
+# functions in proggraming are used to bundle a set of instructions that you want to use repeatedly or that, because of their complexity, are better self-contained in a sub-program and called when
+# counter = 50
+# num = 0
+# while counter > num:
+#     print(counter)
+#     counter = counter + 2
+    # counter = 50
+# num = 0
+# while counter > num:
+#     print(num)
+#     num = num + 1
+
+# names = ["Nika", "Giorgi", "Vano", "Levani, Tea"]
+# names[2] = "Nikoloz"
+# print(names)
