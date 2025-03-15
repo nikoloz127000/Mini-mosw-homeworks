b9f09066ad64da10474a47669ca7559586fcd29c
nikoloz127000 <nicksmith127000@gmail.com>
2025-03-15 15:43:31 +0400

    commit

---
+++ Level 19/Level 20/Homework/py.py
@@ -0,0 +1,14 @@
+# list = []
+# for i in list:
+#     name = input("What is your favorite name?:")
+#     name1 = input("What is your second favorite name?:")
+#     name2 = input("What is your third favorite name?:")
+#     name3 = input("What is your fourth favorite name?:")
+#     name4 = input("What is your fifth  favorite name?:")
+#     list.append(name)
+#     list.append(name1)
+#     list.append(name2)
+#     list.append(name3)
+#     list.append(name4)
+#     print(i)
+    
---
+++ Level 20/Homework/py.py
@@ -0,0 +1,23 @@
+# list = []
+# for i in range(0, 202, 2):
+#     list.append(i)
+#     print(list)
+
+
+
+
+# list = []
+# for i in range(5):
+#     name = input("What is your favorite name?:")
+#     list.append(name)
+# print(list)
+
+# name = "Nikolozi"
+# print(len(name))
+
+# list = [1, 2, 3, 4, 5]
+# list.pop(0)
+# list.insert(0, 6)
+# print(list)
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(len(list))
