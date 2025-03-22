acd1019718a5b0f6dc57aaad9eadc4e005df9e37
nikoloz127000 <nicksmith127000@gmail.com>
2025-03-22 22:03:47 +0400

    commit

---
+++ Level 23/Homework/py.py
@@ -0,0 +1,22 @@
+# def add(a, b):
+#     print(a + b)
+# print(add(100, 12121))
+
+# def number(a):
+    
+#     if number == 0:
+#         print("This number is nagitive")
+#     else:
+#         print("This number is positive")
+        
+
+# print(number())
+
+# def greet():
+#     name = input("Enter your name: ")
+#     print("Hello", name)
+# print(greet())
+
+# def contcatination(a, b):
+#     print(a + b)
+# print(contcatination("Hello", "World"))
